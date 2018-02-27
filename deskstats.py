420d8fd70c75f798cfbfe2d816c81b1b7a4b748a
David Pflug <david@pflug.email>
2018-02-27 13:16:52 -0500

    Rewrite for Python 2
      * Switch from pygal to matplotlib
      * Switch from email.message.EmailMessage to
        email{.mime.multipart,MIMEMultipart}.MIMEMultipart
      * Convert from f-strings to % interpolation
    
      But it works!

--- deskstats.py
+++ deskstats.py
@@ -1,21 +1,39 @@
-#!env python
-import argparse
-import pygal
+from __future__ import print_function
+
+import matplotlib.pyplot as plt
 import subprocess
 import sys
-import tempfile
+#import pygal
+import numpy as np
 
 from collections import Counter
 from datetime import date
-from email.message import EmailMessage
 from email.utils import make_msgid
 from python_rt.rt import Rt
 
-parser = argparse.ArgumentParser(description='Generate CTSS "Week in Review" email')
-parser.add_argument('--date', '-d',
-                    help='Select the week to generate stats for. (YYYY-MM-DD required) (Also, not yet working)')
+# Python 2/3 compatibility
+if sys.version_info[0] == 3:
+    from email.mime.multipart import MIMEMultipart
+    from email.mime.image import MIMEImage
+    from email.mime.text import MIMEText
+    from tempfile import TemporaryDirectory, TemporaryFile
+else:
+    import shutil
 
-args = parser.parse_args()
+    from email.MIMEMultipart import MIMEMultipart
+    from email.MIMEImage import MIMEImage
+    from email.MIMEText import MIMEText
+    from tempfile import mkdtemp, TemporaryFile
+
+    class TemporaryDirectory(object):
+        """Context manager for tempfile.mkdtemp() to duplicate behavior of Python3, letting me using it with "with" keyword."""
+        def __enter__(self):
+            self.name = mkdtemp()
+            return self.name
+
+        def __exit__(self, exc_type, exc_value, traceback):
+            shutil.rmtree(self.name)
+
 
 # No login credentials needed/given because they're in ~/.netrc.
 # For some reason, I can't get login to work any other way. Look into this later, maybe?
@@ -29,9 +47,10 @@ searches = [
     'Created > "-1 week"',
     'Status = "resolved" AND Resolved > "-1 week"',
     'Updated > "-1 week"',
-    f'Created > "-1 week" AND {ticket_open}',
+    'Created > "-1 week" AND %s' % ticket_open,
     ticket_open,
-    f'{ticket_open} AND {stale}']
+    '%s AND %s' % (ticket_open, stale),
+]
 
 search_keys = [
     'CF.{County}',
@@ -49,38 +68,23 @@ email_sections = [
     'Open tickets by Owner',
     'Stale tickets by Owner']
 
-today = date.today()
-
 html = ['<html>',
         '<head>',
         '  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">',
-        # For SVG/PNG alternates
-        #'  <style>',
-        #'    .show {',
-        #'      height: 100% !important;',
-        #'      width: 100% !important;',
-        #'    }',
-        #'',
-        #'    .hide {',
-        #'      display: none;',
-        #'    }',
-        '  </style>',
         '</head>',
         '<body bgcolor="#FFFFFF" text="#000000">']
 
-msg = EmailMessage()
-msg['Subject'] = f'Technology Support Staff Week in Review {today}'
+msg = MIMEMultipart()
+msg['To'] = 'David Pflug <dpflug@circuit5.org>'
+#msg['To'] = 'Court Tech Dept <court_technology_department@circuit5.org>'
 msg['From'] = 'David Pflug <dpflug@circuit5.org>'
-msg['To'] = 'Court Tech Dept <court_technology_department@circuit5.org>'
-#msg['To'] = 'David Pflug <dpflug@circuit5.org>'
-msg.preamble = "This is a multi-part message in MIME format. Please enable html email in your client to view it."
-msg.set_content("This is a multi-part message in MIME format. Please enable html email in your client to view it.")
+msg['Subject'] = 'Technology Support Staff Week in Review %s' % date.today()
 
 def make_table(key, data):
     '''Assumes 2-column data because I don't need anything else.'''
     html = ['<table>',
             '  <tr>',
-            f'    <th>{key}</th>',
+            '    <th>%s</th>' % 'County' if key == 'CF.{County}' else key,
             '    <th>Tickets</th>',
             '  </tr>']
     for i, (c1, c2) in enumerate(data):
@@ -88,14 +92,14 @@ def make_table(key, data):
             html.append('  <tr style="background:#CCC;">')
         else:
             html.append('  <tr>')
-        html.append(f'    <td style="text-align:right;padding-right:0.5em">{c1}</td>')
-        html.append(f'    <td>{c2}</td>')
+        html.append('    <td style="text-align:right;padding-right:0.5em">%s</td>' % c1)
+        html.append('    <td>%s</td>' % c2)
         html.append('  </tr>')
 
     html.append('</table>')
     html.append('<br>') # We need space below our tables
     html.append('<br>')
-    
+
     return '\n'.join(html)
 
 img_cids = []
@@ -104,70 +108,89 @@ imgs = []
 results = []
 
 for search in searches:
+    # Do our searches, collecting the results
     results.append(rt.search(Queue = 'Technology Support', order='Owner', raw_query=search))
 
-with tempfile.TemporaryDirectory() as tempdir:
-    pngs = []
-    #svgs = []
+with TemporaryDirectory() as tempdir:
+    # List of images to add after the fact, so we don't have all our graphs appearing at the top of the email
+    imgs = []
+    # Construct our email
     for sec, search, key, result_list in zip(email_sections, searches, search_keys, results):
         '''Assemble our email sections'''
-        # Count up our ticket totals
+        ## Count up our ticket totals
         totals = Counter()
         for ticket in result_list:
             tkey = ticket.get(key, 'None')
             if tkey == '':
                 tkey = 'None'
-            totals[tkey] += 1
+            if ',' in tkey:  # Mainly happens when multiple counties are assigned the same ticket
+                for subkey in tkey.split(','):
+                    totals[subkey] += 1
+            else:
+                totals[tkey] += 1
 
         sorted_totals = sorted(totals.items(), key=lambda x: x[1], reverse=("tickets by owner" in sec.lower()))
-        
-        #DEBUG
-        print(sorted_totals, file=sys.stderr)
 
-        # Create bar charts
-        chart = pygal.Bar(x_label_rotation=30,
-                          show_legend=False,
-                          rounded_bars=10,
-                          print_values=True,
-                          print_zeroes=False)
-        chart.title = f'{sec}\n{search}'
-        chart.x_labels = [x[0] for x in sorted_totals]
-        chart.add('Tickets', [x[1] for x in sorted_totals])
+        ## DEBUG/printed stats
+        #print(sorted_totals, file=sys.stderr)
+
+        ## Create bar chart
+        xindex = np.arange(len(sorted_totals))
+        padding = 1
+        chart = plt.bar(xindex + padding,
+                        list(x[1] for x in sorted_totals),
+                        align='center',)
+
+        # Label our bars
+        ax = plt.gca()
+        (y_bottom, y_top) = ax.get_ylim()
+        y_height = y_top - y_bottom
+        for bar in chart:
+            height = bar.get_height()
+            p_height = (height / y_height)
+
+            if p_height > 0.98:
+                label_pos = height - (y_height * 0.05)
+            else:
+                label_pos = height + (y_height * 0.01)
+
+            ax.text(bar.get_x() + bar.get_width()/2.0,
+                    label_pos,
+                    '%d' % int(height),
+                    ha='center',
+                    va='bottom',)
+
+        # Various prettiness
+        plt.title("%s\n%s" % (sec, search))
+        plt.margins(0.2)
+        plt.subplots_adjust(bottom=0.3)
+        plt.xticks(xindex + padding,
+                   list(x[0] for x in sorted_totals),
+                   rotation=90)
 
         # Render bar charts
-        _, tf = tempfile.mkstemp(dir=tempdir)
-        chart.render_to_png(tf)
-        cid = make_msgid()[1:-1]
-        #html.append(f'<img class="hide" src="cid:{cid}" />')
-        html.append(f'<img src="cid:{cid}" />')
-        pngs.append((tf, cid))
+        with TemporaryFile(mode='w+b', dir=tempdir) as tf:
+            plt.savefig(tf)
+            tf.flush()
+            tf.seek(0)  # Make sure we're ready for the read() below
+            cid = make_msgid()[1:-1]
+            html.append('<img src="cid:%s" />' % cid)
+            img = MIMEImage(tf.read(), _subtype='png')
+            img.add_header('Content-ID', '<%s>' % cid)
+            imgs.append(img)
 
-        # SVG in email is poorly supported
-        #h, tf = tempfile.mkstemp(dir=tempdir)
-        #print(dir(h))
-        #chart.render_to_file(tf)
-        #cid = make_msgid()[1:-1]
-        #html.append(f'<img class="show" width="0" height="0" src="cid:{cid}" />')
-        #svgs.append((tf, cid))
+        # Clean up after Matplotlib
+        plt.clf()
+        plt.cla()
 
         html.append(make_table(key, sorted_totals))
 
     html.append('</body>')
     html.append('</html>')
-        
-    msg.add_alternative('\n'.join(str(e) for e in html), subtype='html')
 
-    #for png, svg in zip(pngs, svgs):
-    for png in pngs:
-        with open(png[0], 'rb') as i:
-            msg.get_payload()[1].add_related(i.read(),
-                                             'image',
-                                             'png',
-                                             cid=png[1])
-        #with open(svg[0], 'rb') as i:
-        #    msg.get_payload()[1].add_related(i.read(),
-        #                                     'image',
-        #                                     'svg+xml',
-        #                                     cid=svg[1])
+    msg.attach(MIMEText('\n'.join(str(e) for e in html), _subtype='html'))
 
-    print(msg)
+    for img in imgs:
+        msg.attach(img)
+
+    print(msg.as_string())
